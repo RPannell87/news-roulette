2b3cc51adc1c25cf6d6783f32d93c6cdde363253
Robert Pannell <rpannell87@gmail.com>
2019-03-08 21:28:17 -0500

    back to window.focus()

--- newsJavascript.js
+++ newsJavascript.js
@@ -30,7 +30,7 @@ function randomUSLink() {
   var randomNumber = Math.floor(Math.random() * usPoliticsArray.length);
   var newUrl = usPoliticsArray[randomNumber];
   var newUSLink = window.open(newUrl, '_blank');
-  /*newUSLink.focus();*/
+  window.focus();
   location.href = newUSLink;
 }
 /*function randomUSLink() {
